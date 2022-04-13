522f0114aad68c168846cff045a584fde1423307
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2022-04-13 14:19:04 +0900

    :sparkles: Feature : update commit

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import sys
 import matplotlib
 matplotlib.use('Qt5Agg')
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
+
+from ui.main_window import Ui_MainWindow
 
 
 class MplCanvas(FigureCanvasQTAgg):
@@ -16,29 +20,35 @@ class MplCanvas(FigureCanvasQTAgg):
         super(MplCanvas, self).__init__(fig)
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-        toolbar = NavigationToolbar(sc, self)
+        ## setup matplotlib
+        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.canvas.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
 
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(toolbar)
-        layout.addWidget(sc)
+        toolbar = NavigationToolbar(self.canvas, self)
 
-        # Create a placeholder widget to hold our toolbar and canvas.
-        widget = QtWidgets.QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        self.ui.verticalLayout_plot.addWidget(self.canvas)
+        self.ui.verticalLayout_plot.addWidget(toolbar)
 
+        ## connect ui and function
+        self.ui.pushButton_apply.clicked.connect(self.plot)
+
+        ## show this widget
         self.show()
+    
+    def plot(self, data):
+        self.canvas.axes.cla()
+        self.canvas.axes.plot([0,1,2,3,4], [10,1,20,3,40])
+        self.canvas.draw()
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     w = MainWindow()
     app.exec_()
