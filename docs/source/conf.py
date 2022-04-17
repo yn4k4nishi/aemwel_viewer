0b02d3ab3ddf8709a44aec9c19b917673107df84
yn4k4ishi <yn4k4nishi.kit@gmail.com>
2022-04-17 12:57:47 +0900

    test : scikit-rf setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,10 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../plot'))
 
+
+import skrf as rf 
+rf.setup_pylab()
 
 # -- Project information -----------------------------------------------------
 
