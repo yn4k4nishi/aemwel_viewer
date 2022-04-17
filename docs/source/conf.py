17eb70cdb701c4a4cd6b67e96f000f4d66c66bf3
yn4k4ishi <yn4k4nishi.kit@gmail.com>
2022-04-17 12:53:41 +0900

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../../plot/'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../plot'))
 
 
 # -- Project information -----------------------------------------------------
